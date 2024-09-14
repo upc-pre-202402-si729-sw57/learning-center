034ffa69cbcbbb4ed5a520acc98e4f4a6a95de0c
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-14 16:26:40 -0500

    feat(courses): add course service client.

---
+++ src/app/learning/services/course.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import {BaseService} from "../../shared/services/base.service";
+import {Course} from "../model/course.entity";
+
+@Injectable({providedIn: 'root'})
+export class CourseService extends BaseService<Course>{
+
+  constructor() {
+    super();
+    this.resourceEndpoint = '/courses';
+  }
+}
