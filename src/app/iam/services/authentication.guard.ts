4c892a0c5e8c22d404b722821a3af0821e14ac7e
Angel Velasquez <pcisavel@upc.edu.pe>
2024-11-16 17:43:36 -0500

    feat(iam): add authentication guard.

---
+++ src/app/iam/services/authentication.guard.ts
@@ -0,0 +1,25 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from "@angular/core";
+import {AuthenticationService} from "./authentication.service";
+import {map, take} from "rxjs";
+
+/**
+ * Guard to check if user is authenticated
+ * <p>
+ *   If user is not authenticated, it will redirect to sign-in page
+ *   and return false
+ * </p>
+ * @param route The route object that will be used to determine if the user can access the route.
+ * @param state The state object that will be used to determine if the user can access the route.
+ */
+export const authenticationGuard: CanActivateFn = (route, state) => {
+  const authenticationService = inject(AuthenticationService);
+  const router = inject(Router);
+  return authenticationService.isIsSignedIn.pipe(take(1), map(isSignedIn => {
+    if (isSignedIn) return true;
+    else {
+      router.navigate(['/sign-in']).then();
+      return false;
+    }
+  }));
+};
